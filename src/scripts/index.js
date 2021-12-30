7c1f6d4e0cca68205a3c932e6377cda77106ae05
czanni <cristianopzanni@gmail.com>
2021-12-30 17:16:54 +0100

    buttons type adjust

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,23 @@
 //Standalone runtime for Regenerator-compiled generator and async functions.
-import regeneratorRuntime from 'regenerator-runtime';
-import '../styles/main.scss';
+import regeneratorRuntime from "regenerator-runtime";
+import "../styles/main.scss";
 import {
   handleSearch,
   handleLocalStorage,
   addItem,
   handleSuggestions,
-} from './functions';
+} from "./functions";
 
-const searchForm = document.querySelector('.searchForm');
-const addItems = document.querySelector('.itemsListContainer');
-const suggestionButton = document.querySelector('.suggestionButton');
+const searchForm = document.querySelector(".searchForm");
+const addItems = document.querySelector(".addButton");
+const suggestionButton = document.querySelector(".suggestionButton");
 
-searchForm.addEventListener('submit', handleSearch);
-addItems.addEventListener('submit', addItem);
-suggestionButton.addEventListener('click', handleSuggestions);
+searchForm.addEventListener("submit", handleSearch);
+addItems.addEventListener("click", addItem);
+suggestionButton.addEventListener("click", handleSuggestions);
 
-const savedData = JSON.parse(localStorage.getItem('data'));
-const savedItems = JSON.parse(localStorage.getItem('items'));
+const savedData = JSON.parse(localStorage.getItem("data"));
+const savedItems = JSON.parse(localStorage.getItem("items"));
 if (savedData && savedItems) {
   handleLocalStorage(savedData, savedItems);
 } else if (savedData) {
