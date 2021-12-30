2d1da9ce477bb745dd3f4f9a2816df28f97941b9
czanni <cristianopzanni@gmail.com>
2021-12-30 16:01:22 +0100

    Design salved but lost all the ItemsSuggestion new logic :(

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,19 +1,19 @@
-const weatherDailyDetails = document.querySelector('.weatherDailyDetails');
-const weatherHeadline = document.querySelector('.weatherHeadline');
-const searchFormContainer = document.querySelector('.searchFormContainer');
-const weatherContainer = document.querySelector('.forecastContainer');
-const itemsListContainer = document.querySelector('.itemsListContainer');
-const form = document.querySelector('.searchForm');
-const itemsList = document.querySelector('.itemsList');
-const mainHeader = document.querySelector('.mainHeader');
+const weatherDailyDetails = document.querySelector(".weatherDailyDetails");
+const weatherHeadline = document.querySelector(".weatherHeadline");
+const searchFormContainer = document.querySelector(".searchFormContainer");
+const weatherContainer = document.querySelector(".forecastContainer");
+const itemsListContainer = document.querySelector(".itemsListContainer");
+const form = document.querySelector(".searchForm");
+const itemsList = document.querySelector(".itemsList");
+const mainHeader = document.querySelector(".mainHeader");
 
 let listOfItems;
 let arrOfItems;
 
 const itemsSuggestions = {
-  rain: ['Umbrella', 'Rubber Boot'],
-  clouds: ['Jacket', 'Scarf'],
-  clear: ['Short', 'Sunscreen'],
+  rain: ["Umbrella", "Rubber Boot"],
+  clouds: ["Jacket", "Scarf"],
+  clear: ["Short", "Sunscreen"],
 };
 
 export async function handleSearch(e) {
@@ -31,7 +31,7 @@ export async function handleSearch(e) {
     );
     const cityLatLon = await response1.json();
     if (response1.status === 404 || cityLatLon.length === 0) {
-      throw new Error('City not found');
+      throw new Error("City not found");
     }
 
     const lat = cityLatLon[0].lat;
@@ -52,7 +52,7 @@ export async function handleSearch(e) {
 
     try {
       localStorage.setItem(
-        'data',
+        "data",
         JSON.stringify({
           cityName,
           lengthOfStay,
@@ -60,71 +60,16 @@ export async function handleSearch(e) {
         })
       );
     } catch (err) {
-      alert('Cannot write to storage');
+      alert("Cannot write to storage");
     }
 
     // Rendering functions
 
-    weatherContainer.style.display = 'flex';
-    itemsListContainer.style.display = 'flex';
+    weatherContainer.style.display = "flex";
+    itemsListContainer.style.display = "flex";
 
     weatherDailyDataRendering(arrayOfDailyData_LengthOfStay);
     weatherHeadlineRendering(lengthOfStay, cityName);
-
-    // const compiledWeatherData = arrayOfDailyData_LengthOfStay.reduce(
-    //   (acc, obj) => {
-    //     const { max, min } = obj.temp;
-    //     const { main, description } = obj.weather[0];
-
-    //     return {
-    //       sumOfMax: acc.sumOfMax + max,
-    //       sumOfMin: acc.sumOfMin + min,
-    //       arrayOfMain: [...acc.arrayOfMain, main],
-    //       arrayOfDescription: [...acc.arrayOfDescription, description],
-    //     };
-    //   },
-    //   {
-    //     sumOfMax: 0,
-    //     sumOfMin: 0,
-    //     arrayOfMain: [],
-    //     arrayOfDescription: [],
-    //   }
-    // );
-
-    // const { sumOfMax, sumOfMin, arrayOfMain } = compiledWeatherData;
-    // const averageMaxTemp = sumOfMax / lengthOfStay;
-    // const averageMinTemp = sumOfMin / lengthOfStay;
-    // const typesOfSky = arrayOfMain.filter(
-    //   (sky, index) => arrayOfMain.indexOf(sky) === index
-    // );
-
-    // typesOfSky.map((sky) => {
-    //   if (sky === 'Rain') {
-    //     itemsSuggestions.rain.map((item) => {
-    //       return itemsList.insertAdjacentHTML(
-    //         'beforeend',
-    //         `<li> ${item} <button class="deleteButton">x</button></li>`
-    //       );
-    //     });
-    //   } else if (sky === 'Clouds') {
-    //     itemsSuggestions.clouds.map((item) => {
-    //       return itemsList.insertAdjacentHTML(
-    //         'beforeend',
-    //         `<li> ${item} <button class="deleteButton">x</button></li>`
-    //       );
-    //     });
-    //   } else if (sky === 'Clear') {
-    //     itemsSuggestions.clear.map((item) => {
-    //       return itemsList.insertAdjacentHTML(
-    //         'beforeend',
-    //         `<li> ${item} <button class="deleteButton">x</button></li>`
-    //       );
-    //     });
-    //   }
-    //   document.querySelectorAll('.deleteButton').forEach((item) => {
-    //     item.addEventListener('click', removeItem);
-    //   });
-    // });
   } catch (err) {
     alert(err);
   }
@@ -133,26 +78,26 @@ export async function handleSearch(e) {
 }
 
 export const weatherHeadlineRendering = (lengthOfStay, cityName) => {
-  searchFormContainer.style.display = 'none'; // Having the "display none" here instead of inside the handleSearch function, avoids having a blank page while the fetching is not completed.
-  mainHeader.style.display = 'none';
+  searchFormContainer.style.display = "none"; // Having the "display none" here instead of inside the handleSearch function, avoids having a blank page while the fetching is not completed.
+  mainHeader.style.display = "none";
   let cityNameSanitized = cityName
-    .split(' ')
+    .split(" ")
     .map((str) => str[0].toUpperCase() + str.slice(1).toLowerCase())
-    .join(' ');
+    .join(" ");
   weatherHeadline.innerHTML = `<div><h2>The weather in ${cityNameSanitized} <br> for
-  the next ${lengthOfStay} ${lengthOfStay < 2 ? 'Day' : 'Days'}</h2></div>`;
+  the next ${lengthOfStay} ${lengthOfStay < 2 ? "Day" : "Days"}</h2></div>`;
   weatherHeadline.insertAdjacentHTML(
-    'beforeend',
+    "beforeend",
     `<div class="newSearchButtonContainer"><button class="button newSearch" type="button">New Search</button></div>`
   );
-  const newSearchButton = document.querySelector('.newSearch');
-  newSearchButton.addEventListener('click', handleNewSearch);
+  const newSearchButton = document.querySelector(".newSearch");
+  newSearchButton.addEventListener("click", handleNewSearch);
 };
 
 export const weatherDailyDataRendering = (arrayOfDailyData_LengthOfStay) => {
   let dayCount = 0;
 
-  weatherDailyDetails.innerHTML = '';
+  weatherDailyDetails.innerHTML = "";
   arrayOfDailyData_LengthOfStay.forEach((day) => {
     const { max, min } = day.temp;
     const { main, description, icon } = day.weather[0];
@@ -160,12 +105,12 @@ export const weatherDailyDataRendering = (arrayOfDailyData_LengthOfStay) => {
     dayCount += 1;
     // console.log(day.weather[0]);
     weatherDailyDetails.insertAdjacentHTML(
-      'beforeend',
+      "beforeend",
       `<div class="infoByDay">
                <div class="descriptionWithImage"><img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather icon"> <p> <span class="alert"> Day ${dayCount}</span> <br> ${
-        main === 'Rain'
+        main === "Rain"
           ? `with ${description}.`
-          : main === 'Clouds'
+          : main === "Clouds"
           ? `with ${description} but without rain.`
           : `with ${description}.`
       }</p></div>
@@ -173,7 +118,7 @@ export const weatherDailyDataRendering = (arrayOfDailyData_LengthOfStay) => {
         <p class="temp"><span class="bold">Temperature:</span> <br>from ${min}°C to ${max}°C and humidity of ${humidity}%.</p>
        
         <p class="wind"><span class="bold">Wind speed:</span> ${wind_speed}m/s. <br> That means a ${
-        wind_speed < 3 ? 'weak' : wind_speed < 6 ? 'mild' : 'strong'
+        wind_speed < 3 ? "weak" : wind_speed < 6 ? "mild" : "strong"
       } wind.</p>
         </div>`
     );
@@ -181,8 +126,8 @@ export const weatherDailyDataRendering = (arrayOfDailyData_LengthOfStay) => {
 };
 
 export const handleSuggestions = () => {
-  const savedData = JSON.parse(localStorage.getItem('data'));
-  const savedItemsLocalStorage = JSON.parse(localStorage.getItem('items'));
+  const savedData = JSON.parse(localStorage.getItem("data"));
+  const savedItemsLocalStorage = JSON.parse(localStorage.getItem("items"));
   let savedItems = [];
   if (savedItemsLocalStorage) {
     savedItems = savedItemsLocalStorage.map((item) => item.trim());
@@ -208,40 +153,40 @@ export const handleSuggestions = () => {
   );
 
   typesOfSky.map((sky) => {
-    if (sky === 'Rain') {
+    if (sky === "Rain") {
       itemsSuggestions.rain.map((item) => {
         if (!savedItems.includes(item)) {
           return itemsList.insertAdjacentHTML(
-            'beforeend',
+            "beforeend",
             `<li> ${item} <button class="deleteButton">x</button></li>`
           );
         }
       });
-    } else if (sky === 'Clouds') {
+    } else if (sky === "Clouds") {
       itemsSuggestions.clouds.map((item) => {
         if (!savedItems.includes(item)) {
           return itemsList.insertAdjacentHTML(
-            'beforeend',
+            "beforeend",
             `<li> ${item} <button class="deleteButton">x</button></li>`
           );
         }
       });
-    } else if (sky === 'Clear') {
+    } else if (sky === "Clear") {
       itemsSuggestions.clear.map((item) => {
         if (!savedItems.includes(item)) {
           return itemsList.insertAdjacentHTML(
-            'beforeend',
+            "beforeend",
             `<li> ${item} <button class="deleteButton">x</button></li>`
           );
         }
       });
     }
-    document.querySelectorAll('.deleteButton').forEach((item) => {
-      item.addEventListener('click', removeItem);
+    document.querySelectorAll(".deleteButton").forEach((item) => {
+      item.addEventListener("click", removeItem);
     });
   });
 
-  listOfItems = document.getElementsByTagName('li');
+  listOfItems = document.getElementsByTagName("li");
   arrOfItems = Array.from(listOfItems);
   saveToLocalStorage();
 };
@@ -251,19 +196,19 @@ export const addItem = (e) => {
   let itemValue = e.target.elements.addItems.value;
 
   if (!itemValue) {
-    alert('Please insert an Item in the input field');
+    alert("Please insert an Item in the input field");
   } else {
     itemsList.insertAdjacentHTML(
-      'beforeend',
+      "afterbegin",
       `<li> ${itemValue} <button class="deleteButton">x</button></li>`
     );
-    document.querySelectorAll('.deleteButton').forEach((item) => {
-      item.addEventListener('click', removeItem);
+    document.querySelectorAll(".deleteButton").forEach((item) => {
+      item.addEventListener("click", removeItem);
     });
-    listOfItems = document.getElementsByTagName('li');
+    listOfItems = document.getElementsByTagName("li");
     arrOfItems = Array.from(listOfItems);
     saveToLocalStorage();
-    e.target.elements.addItems.value = '';
+    e.target.elements.addItems.value = "";
     console.log(arrOfItems);
   }
 };
@@ -282,11 +227,6 @@ const removeItem = (e) => {
 
 export const handleNewSearch = (e) => {
   e.preventDefault();
-  // weatherContainer.style.display = 'none';
-  // itemsListContainer.style.display = 'none';
-  // searchFormContainer.style.display = 'block';
-  // mainHeader.style.display = 'block';
-  // arrOfItems;
   localStorage.clear();
   window.location.reload();
 };
@@ -297,9 +237,9 @@ const saveToLocalStorage = () => {
     arrOfValues.push(item.firstChild.data);
   });
   try {
-    localStorage.setItem('items', JSON.stringify(arrOfValues));
+    localStorage.setItem("items", JSON.stringify(arrOfValues));
   } catch (err) {
-    alert('Cannot write to storage');
+    alert("Cannot write to storage");
   }
 };
 
@@ -307,20 +247,20 @@ export const handleLocalStorage = (savedData, savedItems) => {
   const { cityName, lengthOfStay, arrayOfDailyData_LengthOfStay } = savedData;
   weatherDailyDataRendering(arrayOfDailyData_LengthOfStay);
   weatherHeadlineRendering(lengthOfStay, cityName);
-  weatherContainer.style.display = 'flex';
-  itemsListContainer.style.display = 'flex';
+  weatherContainer.style.display = "flex";
+  itemsListContainer.style.display = "flex";
   if (savedItems) {
     savedItems.map((item) => {
       return itemsList.insertAdjacentHTML(
-        'beforeend',
+        "beforeend",
         `<li> ${item} <button class="deleteButton">x</button></li>`
       );
     });
   }
 
-  document.querySelectorAll('.deleteButton').forEach((item) => {
-    item.addEventListener('click', removeItem);
+  document.querySelectorAll(".deleteButton").forEach((item) => {
+    item.addEventListener("click", removeItem);
   });
-  listOfItems = document.getElementsByTagName('li');
+  listOfItems = document.getElementsByTagName("li");
   arrOfItems = Array.from(listOfItems);
 };
